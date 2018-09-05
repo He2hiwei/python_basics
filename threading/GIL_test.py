b067ffb666343efcba24ba2d7cd1ec42dcae3b2a
NDHoHo <2832079925@qq.com>
2018-09-05 20:41:12 +0800

    write GIL_test.py

--- threading/GIL_test.py
+++ threading/GIL_test.py
@@ -6,3 +6,30 @@ import time
 def job(l,q):
     res = sum(l)
     q.put(res)
+
+def multithreading(l):
+    q = Queue()
+    threds = []
+
+    for i in range(4):
+        t = threading.Thread(target=job,args=(l,q))
+        t.start()
+        threds.append(t)
+    [t.join() for t in threds]
+    total = 0
+    for _ in range(4):
+        total += q.get()
+    print(total)
+
+def normal(l):
+    total = sum(l)
+    print(total)
+    
+if __name__ == '__main__':
+    l = list(range(1000000))
+    s_t = time.time()
+    normal(l*4)
+    print('normal:',time.time()-s_t)
+    s_t = time.time()
+    multithreading(l)
+    print('multithreading: ',time.time()-s_t)
